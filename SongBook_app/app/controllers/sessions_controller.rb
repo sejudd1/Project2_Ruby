08b4571d5cd0f74a07866c5a1a735f56eff258df
Steve Judd <sejudd1@github.com>
2015-09-14 13:07:27 -0700

    creates controllers routes and views for Log In and Sign In pages to complete user authentication

---
+++ SongBook_app/app/controllers/sessions_controller.rb
@@ -0,0 +1,27 @@
+class SessionsController < ApplicationController
+
+	def new
+	end
+
+	def create
+		user = User.find_by(email: params[:login][:email])
+		if user && user.authentication(params[:login][:password])
+			#setting a cookie to verify user is the user signing in
+			#make a new key in the session object 
+			#which gets stored in a cookie
+			session[:user_id] = user.id.to_s
+			redirect_to users_path
+			#turning user_id into a string because cookies can only store strings
+		else
+			#something's wrong please try again?
+			render :new
+
+		end
+	end
+
+	def destroy
+		session.delete(:user_id)
+		redirect_to login_path
+	end
+
+end
--- SongBook_app/config/routes.rb
+++ SongBook_app/config/routes.rb
@@ -1,4 +1,15 @@
 Rails.application.routes.draw do
+
+  get "users" => "users#index"
+  get "users/new" => "users#new"
+  post "users" => "users#create"
+  get "/login" => "sessions#new"
+  post "/login" => "sessions#create"
+  delete "/logout" => "sessions#destroy"
+
+
+
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
